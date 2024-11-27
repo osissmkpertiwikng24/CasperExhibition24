7fc655e7b45746bb3c3680da86b6c8a5f9a6785a
osissmkpertiwikng24 <osiscasper79@gmail.com>
2024-11-27 09:25:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Open Registration Popup
+function openRegistrationPopup() {
+  document.getElementById('registrationPopup').style.display = 'flex';
+}
+
+// Close Registration Popup
+function closeRegistrationPopup() {
+  document.getElementById('registrationPopup').style.display = 'none';
+}
+
+// Open Slide Page
+function openSlidePage() {
+  document.getElementById('slidePage').style.display = 'block';
+}
+
+// Close Slide Page
+function closeSlidePage() {
+  document.getElementById('slidePage').style.display = 'none';
+}
